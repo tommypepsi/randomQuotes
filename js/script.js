572d9f835b2a586a38e833595003e34609fdce9e
tommypepsi <n1tommy_pepsi@live.ca>
2017-03-01 00:44:37 -0500

    test avec script Vibrant.js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,67 @@
 'use strict';
+var windowWidth = $(window).width();
+var windowHeight = $(window).height();
+//swatches is a global variable so that we can set it when the background is called and use it when we click the button
+var swatches;
 $(document).ready(function () {
 
-  let quotes = $.getJSON("http://quotes.stormconsultancy.co.uk/quotes.json", function (json)
-  {
-    let random = Math.floor(Math.random() * json.length)
-    let quote = json[random].quote
-    let author = " —"  + json[random].author
-    let quoteTweet = truncateString(quote, 140 - author.length)
-    quoteTweet += author
+  newQuote()
 
-    $("#quotes").html(quote)
-    $("#twitter").attr("href", "https://twitter.com/intent/tweet?text=" + quoteTweet)
-    $("#next").on("click", function ()
-    {
-      let random = Math.floor(Math.random() * json.length)
-      let quote = json[random].quote
-      let quoteTweet = truncateString(quote, 140);
-      $("#quotes").html(quote)
-      $("#twitter").attr("href", "https://twitter.com/intent/tweet?text=" + quoteTweet)
-    })
+  $("#next").click(function(){
+    newQuote()
+
+    //we put the dominant color of the last background as the loading background
+    $("body").css("background-color", swatches["DarkVibrant"].getHex())
+    //and style the loader with the other colors from vibrant script
+    $("#loader").css("border", "5px solid " + swatches["Muted"].getHex())
+    $("#loader").css("border-top", "5px solid " + swatches["Vibrant"].getHex())
+
   })
 
-
-
 })
+
+function newQuote(){
+
+  //we show a loading gif while we wait for the quote
+  $("#wrapper").css("opacity", "0")
+
+  //We call a new background image
+  var newBackground = new Image()
+  //The random is to make the image change
+  newBackground.src = "https://unsplash.it/"+ windowWidth +"/"+ windowHeight +"/?random&test=" + Math.random()
+  newBackground.alt = ""
+  newBackground.id = "background"
+
+  //and when it is loaded we call the quote
+  newBackground.onload = function() {
+    //Vibrant is a function from a script that gets dominant color
+    var vibrant = new Vibrant(newBackground);
+    swatches = vibrant.swatches()
+
+    //cache needs to be false or the quotes won't change
+    $.ajaxSetup({ cache: false});
+    $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", function (json)
+    {
+      let quote = json[0].content
+      let author = " —"  + json[0].title
+      let quoteTweet = truncateString(quote, 140 - author.length)
+      quoteTweet += author
+
+      $("#quote").html(quote)
+      $("#author").html(author)
+      $("#twitter").attr("href", "https://twitter.com/intent/tweet?text=" + quoteTweet)
+
+
+      $("#wrapper").css("opacity", "1")
+      $("#next").css("background-color", swatches["LightVibrant"].getHex())
+      $("#next").css("color", swatches["DarkMuted"].getHex())
+
+      $("#imgBackground").html(newBackground);
+    })
+  }
+
+
+}
 
 function truncateString(str, num) {
   // Clear out that junk in your trunk
